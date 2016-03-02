1353d4e0a9221cb76f59c30bff319e34185d00bb
rs7 <rs7xxx@gmail.com>
2016-03-02 14:57:49 +0600

    Добавлено отображение даты обнаружения события

--- client/public/util.js
+++ client/public/util.js
@@ -18,3 +18,42 @@ function getUrlVars() {
     );
     return vars;
 }
+
+function printDate(date) {
+    var boundary;
+
+    //пять минут назад
+    boundary = new Date();
+    boundary.setMinutes(boundary.getMinutes() - 5);
+    if (date >= boundary) {
+        return 'только что';
+    }
+
+    //начало сегодняшнего дня
+    boundary = new Date();
+    boundary.setHours(0, 0, 0, 0);
+    if (date >= boundary) {
+        return 'сегодня';
+    }
+
+    //начало вчерашнего дня
+    boundary = new Date();
+    boundary.setDate(boundary.getDate() - 1);
+    boundary.setHours(0, 0, 0, 0);
+    if (date >= boundary) {
+        return 'вчера';
+    }
+
+    var months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
+
+    //начало года
+    boundary = new Date();
+    boundary.setMonth(0, 1);
+    boundary.setHours(0, 0, 0, 0);
+    if (date >= boundary) {
+        return date.getDate() + ' ' + months[date.getMonth()];
+    }
+
+    //в прошлом году и старше
+    return date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
+}
