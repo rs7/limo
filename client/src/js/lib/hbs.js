39e71bc6aec193c722f8dc25303c6310577d6fe4
rs7 <rs7xxx@gmail.com>
2016-03-06 21:25:58 +0600

    Корректные ссылки в итемах
    Добавлены хелперы в шаблоны для упрощения вывода информации

--- client/src/js/lib/hbs.js
+++ client/src/js/lib/hbs.js
@@ -6,4 +6,10 @@ export function initHelpers() {
     Handlebars.registerHelper("timestamp", date => timestamp(date));
 
     Handlebars.registerHelper("date", date => printDate(date));
+
+    Handlebars.registerHelper("user_link", ({domain}) => `//vk.com/${domain}`);
+
+    Handlebars.registerHelper("photo_link", ({owner_id, id}) => `//vk.com/photo${owner_id}_${id}`);
+
+    Handlebars.registerHelper("user_name", ({first_name, last_name}) => `${first_name} ${last_name}`);
 }
