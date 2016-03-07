e01568cf7429de0cfc11b14107607cdc17330bf0
rs7 <rs7xxx@gmail.com>
2016-03-07 17:47:13 +0600

    Добавлено поведение при попадании в список удалённых фотографий

--- client/src/js/lib/model.js
+++ client/src/js/lib/model.js
@@ -1,9 +1,35 @@
 import * as request from './request';
 
+import {user} from './params';
+
 export let getPhotos = request.getPhotos;
 
 export function getPhotosByList(photos) {
-    return (photos.length > 0) ? request.getPhotosByList(photos) : [];
+    if (photos.length == 0) {
+        return [];
+    }
+
+    return request.getPhotosByList(photos).catch(error => {
+        if (error.error_code == 200) {
+            //Код 200 возвращается, если все фотографии в запрашиваемом списке удалены
+            return [];
+        }
+
+        throw error;
+    }).then(responsePhotos => {
+        //В запрашиваемом списке были удалённые фотографии, добавляем заглушку вместо них
+        if (responsePhotos.length < photos.length) {
+            let responsePhotosList = responsePhotos.map(photo => photo.id);
+            let deletedPhotos = photos.filter(photo => !responsePhotosList.includes(photo));
+            Array.prototype.push.apply(responsePhotos, deletedPhotos.map(photo => ({
+                id:photo,
+                owner_id:user,
+                photo_130:'//vk.com/images/camera_100.png'
+            })));
+        }
+
+        return responsePhotos;
+    });
 }
 
 export let getLikes = request.getLikes;
