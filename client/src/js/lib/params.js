bc27b025145f41452b6cc11482295bc74cee6b47
rs7 <rs7xxx@gmail.com>
2016-03-07 21:47:16 +0600

    Тип параметра userId приведён к числу

--- client/src/js/lib/params.js
+++ client/src/js/lib/params.js
@@ -4,6 +4,6 @@ const runParams = getUrlVars();
 
 export let authKey = runParams.auth_key;
 
-export let user = runParams.user_id;
+export let user = parseInt(runParams.user_id);
 
 export let protocol = ['http', 'https'][runParams.is_secure];
