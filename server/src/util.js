1c81a676c3d50c0790caecdd28e852c301d97d47
rs7 <rs7xxx@gmail.com>
2016-03-14 18:00:01 +0600

    Функция sliceObject для вычленения части объекта по списку ключей

--- server/src/util.js
+++ server/src/util.js
@@ -7,3 +7,9 @@ import {appId, secretKey} from './config';
 export function authCheck(user, authKey) {
     return authKey === md5(`${appId}_${user}_${secretKey}`);
 }
+
+export function sliceObject(object, keys) {
+    let slice = {};
+    keys.forEach(key => slice[key] = object[key]);
+    return slice;
+}
