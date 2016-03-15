6df9baee9c92209d8feaba6093c14e0ebc97ac53
rs7 <rs7xxx@gmail.com>
2016-03-15 18:13:16 +0600

    Изменён strict-режим в mongoose-схемах

--- server/src/db.js
+++ server/src/db.js
@@ -7,12 +7,18 @@ mongoose.connect('mongodb://localhost/limo');
 let likesSchema = mongoose.Schema({
     photo: {type: Number, required: true},
     likes: {type: [Number], default: []}
-}, {strict: true, _id: false});
+}, {
+    strict: 'throw',
+    _id: false
+});
 
 let snapshotSchema = mongoose.Schema({
     date: {type: Date, required: true},
     items: {type: [likesSchema], default: []}
-}, {strict: true, _id: false});
+}, {
+    strict: 'throw',
+    _id: false
+});
 
 let feedSchema = mongoose.Schema({
     photo: {type: Number, required: true},
@@ -21,13 +27,19 @@ let feedSchema = mongoose.Schema({
         from: {type: Date, required: true},
         to: {type: Date, required: true}
     }
-}, {strict: true, _id: false});
+}, {
+    strict: 'throw',
+    _id: false
+});
 
 let userSchema = mongoose.Schema({
     id: {type: Number, required: true},
     snapshot: {type: snapshotSchema, required: true},
     last_seen: {type: Date, required: true},
     feeds: {type: [feedSchema], default: []}
-}, {strict: true, id: false});
+}, {
+    strict: 'throw',
+    id: false
+});
 
 export let User = mongoose.model('User', userSchema);
